cf27c66309dd67ff4018325f00507d5161fdcb2c
Timothy H <huynh.tk80@gmail.com>
2022-11-15 16:13:10 -0700

    auto redirect

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -5,7 +5,7 @@ function Notfound() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const timeout = setTimeout(() => navigate("home"), 1000);
+    const timeout = setTimeout(() => navigate("/home"), 2000);
 
     return () => {
       clearTimeout(timeout);
@@ -19,6 +19,7 @@ function Notfound() {
           <h3 className="title flex-title">
             <span className="title-main">
               <h1>404 Page Not Found</h1>
+              <p>Redirecting to home...</p>
             </span>
           </h3>
         </div>
