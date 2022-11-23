61b8bdd562b0ada01e0f8f3bbf03d9a361e98c63
Timothy H <huynh.tk80@gmail.com>
2022-11-23 13:59:37 -0700

    removed old website

--- potlucker-version-2/navbar1.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-var $siteHeaderItems = $('.siteHeader .siteHeader__item');
-
-$siteHeaderItems.click((event) => {
-    var selectedClass = 'is-site-header-item-selected';
-    $siteHeaderItems.removeClass(selectedClass);
-    $(event.target).addClass(selectedClass);
-});
