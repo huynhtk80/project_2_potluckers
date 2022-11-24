5ee5aac7128f8b0f5aa3a238b7d3ebc96ff9b410
steakncheese <romell.bermundo@outlook.com>
2022-11-24 16:54:38 -0700

    Added section in about us

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -11,25 +11,13 @@ function Aboutus() {
               <h1>About Us</h1>
             </span>
             <p>
-              About us - hjfasdkjhLorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Gravida cum sociis natoque penatibus et. Velit laoreet id donec
-              ultrices tincidunt arcu non. Ut etiam sit amet nisl purus.
-              Curabitur gravida arcu ac tortor dignissim. Nulla porttitor massa
-              id neque aliquam. Bibendum arcu vitae elementum curabitur vitae
-              nunc. Neque sodales ut etiam sit amet nisl purus in. Ac tincidunt
-              vitae semper quis lectus nulla. Praesent elementum facilisis leo
-              vel. Mi in nulla posuere sollicitudin aliquam. Montes nascetur
-              ridiculus mus mauris. Tincidunt dui ut ornare lectus sit amet.
-              Sollicitudin nibh sit amet commodo nulla facilisi nullam. Mauris
-              augue neque gravida in fermentum et sollicitudin. Mi proin sed
-              libero enim sed faucibus. Duis at consectetur lorem donec massa
-              sapien. Venenatis tellus in metus vulputate eu scelerisque felis.
-              Vel risus commodo viverra maecenas accumsan. Dictum fusce ut
-              placerat orci. Ligula ullamcorper malesuada proin libero nunc
-              consequat interdum varius. Viverra justo nec ultrices dui. Elit
-              sed vulputate mi sit amet. Sed sed risus pretium.{" "}
-              <Link to="/home/PlanYourOwnEvent">Plan Your Own Event</Link>
+            About Potlockers
+Organizing potlucks is hard. You need to know allergies, preferences, cutlery, clean-up crew and much more. Life is already hard but organizing potlock doesn’t have to be. What if we tell you that there is an app to make your life easier? Well, we present to you the Potlockers app. This application is made by the Kamikaze Cheese Team and our goal is to make organizing potlucks easy and convenient. Coming from the struggles we have on creating potluck during our study in InceptionU, it is our mission to deliver food goodness to everybody in a quick and easy manner.
+</p><br /><p>The art of good Food</p><br /><p>
+Our team core value is food and doing good. The Potlockers app will allow you to add/remove foods from your potlock list. In addition, you can look up recipes for the food your are bringing, organize who brings in the cutlery, organize who cleans-up after the event, exclude certain types of food because of allergies and other additional features. We hope that by using our app, eating food with the people you cherish can create great experiences they can never forget. Bon appétit!
+</p><br /><p>Good Food. Combine.
+{" "}
+</p><br /><p><Link to="/home/PlanYourOwnEvent">Plan Your Own Event</Link>
             </p>
           </h3>
         </div>
