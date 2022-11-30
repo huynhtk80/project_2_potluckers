dafa51aa99a3a53fe1b0325355b985f913d39329
Timothy H <huynh.tk80@gmail.com>
2022-11-30 12:07:59 -0700

    removed main.yml action

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -9,57 +9,82 @@ import hgroup from "../img/potlucker-header-image.jpg";
 function Aboutus() {
   return (
     <div className="aboutus-container">
-    <div className="aboutus-firstbox">      
-      <div className="aboutus-main">
-        <span className="title-main"><h1>Potluck made easy</h1></span>
-            <br />
-            <p><i>One cannot think well, love well, sleep well, if one has not dined well.</i> Virginia Woolf</p>
-            <br />
-            <p>Organizing potlucks can be challenging. You need to know
-              allergies, preferences, cutlery, clean-up crew and much more. Life
-              is already hard but organizing potlock does not have to be. What if
-              we tell you that there is an app to make your life easier? Well,
-              we present to you the Potlockers app. This application is made by
-              the Kamikaze Cheese Team and our goal is to make organizing
-              potlucks easy and convenient. Coming from the struggles we have on
-              creating potluck during our study in InceptionU, it is our mission
-              to deliver food goodness to everybody in a quick and easy manner.</p>
-            <br />
+      <div className="aboutus-firstbox">
+        <div className="aboutus-main">
+          <span className="title-main">
+            <h1>Potluck made easy</h1>
+          </span>
+          <br />
+          <p>
+            <i>
+              One cannot think well, love well, sleep well, if one has not dined
+              well.
+            </i>{" "}
+            Virginia Woolf
+          </p>
+          <br />
+          <p>
+            Organizing potlucks can be challenging. You need to know allergies,
+            preferences, cutlery, clean-up crew and much more. Life is already
+            hard but organizing potlock does not have to be. What if we tell you
+            that there is an app to make your life easier? Well, we present to
+            you the Potlockers app. This application is made by the Kamikaze
+            Cheese Team and our goal is to make organizing potlucks easy and
+            convenient. Coming from the struggles we have on creating potluck
+            during our study in InceptionU, it is our mission to deliver food
+            goodness to everybody in a quick and easy manner.
+          </p>
+          <br />
+        </div>
+        <div className="aboutus-sidebar">
+          <img
+            src={hstew}
+            alt="Stew"
+            width={600}
+            height={700}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </div>
 
-      </div>
-      <div className="aboutus-sidebar">
-      <img src={hstew} alt="Stew" width={600} height={700} style={{objectFit:"cover"}}/>
-      </div>
-      </div>
-
-
-    <div className="aboutus-secondbox">
-      <div className="aboutus-sidebar2">
-      <img src={hgroup} alt="group" width={600} height={700} style={{objectFit:"cover"}}/>
-      </div>
-      <div className="aboutus-main2">
-        <span className="title-main"><h1>The art of good Food</h1></span>
+      <div className="aboutus-secondbox">
+        <div className="aboutus-sidebar2">
+          <img
+            src={hgroup}
+            alt="group"
+            width={600}
+            height={700}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="aboutus-main2">
+          <span className="title-main">
+            <h1>The art of good Food</h1>
+          </span>
+          <br />
+          <p>
+            Our team core value is food and doing good. The Potlockers app will
+            allow you to add/remove foods from your potlock list. In addition,
+            you can look up recipes for the food you are bringing, organize who
+            brings in the cutlery, organize who cleans-up after the event,
+            exclude certain types of food because of allergies and other
+            additional features. We hope that by using our app, eating food with
+            the people you cherish can create great experiences they can never
+            forget. Bon appétit!
+          </p>
+          <br />
+          {/* <p>Good Food. Combine. </p> */}
+          {/* <br /> */}
+          <p>
             <br />
-            <p>Our team core value is food and doing good. The Potlockers app
-              will allow you to add/remove foods from your potlock list. In
-              addition, you can look up recipes for the food you are bringing,
-              organize who brings in the cutlery, organize who cleans-up after
-              the event, exclude certain types of food because of allergies and
-              other additional features. We hope that by using our app, eating
-              food with the people you cherish can create great experiences they
-              can never forget. Bon appétit!</p>
-            <br />
-            {/* <p>Good Food. Combine. </p> */}
-            {/* <br /> */}
-            <p>
-              <br />
-              <Link to="/home/PlanYourOwnEvent" className="btn btn--green">Plan Your Own Event</Link>
-            </p>
-      </div>
+            <Link to="/home/PlanYourOwnEvent" className="btn btn--green">
+              Plan Your Own Event
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Aboutus;
-
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,74 +1,114 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Index() {
   return (
-      <div className="container">
-            <span className="title-main">
-              <h1>HOME PAGE</h1>
-            </span>
-            <p>
-              Dashboard Homepage -Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Gravida cum sociis natoque penatibus et. Velit laoreet id donec
-              ultrices tincidunt arcu non. Ut etiam sit amet nisl purus.
-              Curabitur gravida arcu ac tortor dignissim. Nulla porttitor massa
-              id neque aliquam. Bibendum arcu vitae elementum curabitur vitae
-              nunc. Neque sodales ut etiam sit amet nisl purus in. Ac tincidunt
-              vitae semper quis lectus nulla. Praesent elementum facilisis leo
-              vel. Mi in nulla posuere sollicitudin aliquam. Montes nascetur
-              ridiculus mus mauris. Tincidunt dui ut ornare lectus sit amet.
-              Sollicitudin nibh sit amet commodo nulla facilisi nullam. Mauris
-              augue neque gravida in fermentum et sollicitudin. Mi proin sed
-              libero enim sed faucibus. Duis at consectetur lorem donec massa
-              sapien. Venenatis tellus in metus vulputate eu scelerisque felis.
-              Vel risus commodo viverra maecenas accumsan. Dictum fusce ut
-              placerat orci. Ligula ullamcorper malesuada proin libero nunc
-              consequat interdum varius. Viverra justo nec ultrices dui. Elit
-              sed vulputate mi sit amet. Sed sed risus pretium.
-            </p>
-            <p>
-              <br />
-              liksjdflksdfjLorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua. Gravida cum sociis natoque penatibus et. Velit laoreet id
-              donec ultrices tincidunt arcu non. Ut etiam sit amet nisl purus.
-              Curabitur gravida arcu ac tortor dignissim. Nulla porttitor massa
-              id neque aliquam. Bibendum arcu vitae elementum curabitur vitae
-              nunc. Neque sodales ut etiam sit amet nisl purus in. Ac tincidunt
-              vitae semper quis lectus nulla. Praesent elementum facilisis leo
-              vel. Mi in nulla posuere sollicitudin aliquam. Montes nascetur
-              ridiculus mus mauris. Tincidunt dui ut ornare lectus sit amet.
-              Sollicitudin nibh sit amet commodo nulla facilisi nullam. Mauris
-              augue neque gravida in fermentum et sollicitudin. Mi proin sed
-              libero enim sed faucibus. Duis at consectetur lorem donec massa
-              sapien. Venenatis tellus in metus vulputate eu scelerisque felis.
-              Vel risus commodo viverra maecenas accumsan. Dictum fusce ut
-              placerat orci. Ligula ullamcorper malesuada proin libero nunc
-              consequat interdum varius. Viverra justo nec ultrices dui. Elit
-              sed vulputate mi sit amet. Sed sed risus pretium.
-            </p>
-            <p>
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Gravida cum sociis natoque penatibus et. Velit laoreet id donec
-              ultrices tincidunt arcu non. Ut etiam sit amet nisl purus.
-              Curabitur gravida arcu ac tortor dignissim. Nulla porttitor massa
-              id neque aliquam. Bibendum arcu vitae elementum curabitur vitae
-              nunc. Neque sodales ut etiam sit amet nisl purus in. Ac tincidunt
-              vitae semper quis lectus nulla. Praesent elementum facilisis leo
-              vel. Mi in nulla posuere sollicitudin aliquam. Montes nascetur
-              ridiculus mus mauris. Tincidunt dui ut ornare lectus sit amet.
-              Sollicitudin nibh sit amet commodo nulla facilisi nullam. Mauris
-              augue neque gravida in fermentum et sollicitudin. Mi proin sed
-              libero enim sed faucibus. Duis at consectetur lorem donec massa
-              sapien. Venenatis tellus in metus vulputate eu scelerisque felis.
-              Vel risus commodo viverra maecenas accumsan. Dictum fusce ut
-              placerat orci. Ligula ullamcorper malesuada proin libero nunc
-              consequat interdum varius. Viverra justo nec ultrices dui. Elit
-              sed vulputate mi sit amet. Sed sed risus pretium.
-            </p>
+    <section className="section-tours">
+      <h1>Learn More About Potlucker</h1>
+
+      <div className="row">
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--7">&nbsp;</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--1">
+                  Keeps Track Of Progress{" "}
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-1">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">
+                    Tracks Everyone's Progress?
+                  </p>
+                  <p className="card__price-value">Fantastic!</p>
+                </div>
+                <a href="#popup7" className="btn btn--white">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--8">&nbsp;</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--2">
+                  Keeps Everyone Informed
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-2">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Everyone's Informed?</p>
+                  <p className="card__price-value">So Great!</p>
+                </div>
+                <a href="#popup8" className="btn btn--white">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture--9">&nbsp;</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span--3">
+                  Creates Your Perfect Menus
+                </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-3">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">The Perfect Menu?</p>
+                  <p className="card__price-value">Spectacular!</p>
+                </div>
+                <a href="#popup9" className="btn btn--white">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </section>
   );
 }
 
